159736422708021f00e2666a05dada60bf675575
zjx <zjxcyr@qq.com>
2022-06-02 09:24:58 +0800

    add users app add superadmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+# 首先引入User
+from django.contrib.auth.models import User
+# 我们看到的这个用户选项就是官方默认通过UserAdmin这个类注册到后台的，那么我们也引入进来，后边继承这个类
+from django.contrib.auth.admin import UserAdmin
+# 再引入我们定义的模型
+from .models import UserProfile
+# 必须先通过unregister将User取消注册
+admin.site.unregister(User)
+
+# 定义关联对象的样式，StackedInline为纵向排列每一行，TabularInline为并排排列
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile   # 关联的模型
+
+# 关联字段在User之内编辑，关联进来
+class UserProfileAdmin(UserAdmin):
+    inlines = [UserProfileInline]
+
+# 重新注册User
+admin.site.register(User, UserProfileAdmin)
